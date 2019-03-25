314860166e05589fe05b451ccfad3c40a5335f5b
Skyner <reskyner@googlemail.com>
2019-03-25 14:55:54 +0000

    small clean-up

--- fragalysis_preproc/graph.py
+++ fragalysis_preproc/graph.py
@@ -20,6 +20,7 @@ class GraphRequest:
         # get full url
         self.search_url = str(self.frag_url + self.graph_url + self.query)
 
+        # set blanks for smiles search and json to handle later
         self.smiles_url = None
         self.graph_json = None
 
@@ -30,12 +31,13 @@ class GraphRequest:
     def get_graph_json(self):
         # check for a smiles url
         if not self.smiles_url:
-            raise Exception('Please initiate smiles url with get_smiles_url(<smiles>)!')
+            raise Exception('Please initiate smiles url with set_smiles_url(<smiles>)!')
 
         # get response from url and decode -> json
         with urllib.request.urlopen(self.smiles_url) as f:
             response = json.loads(f.read().decode('utf-8'))
 
+        # set json as decoded response for processing
         self.graph_json = response
 
         return response
